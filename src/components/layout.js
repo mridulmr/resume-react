680dae845f17404e665f6c66bdb21a6faf2c67b9
akkuu <mridulmr@outlook.com>
2023-11-08 13:31:58 +0530

    updated the code

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 import ScrollTop from '../elements/scrollTop';
-import "swiper/css";
+import 'swiper/css';
+import DotRing from '../elements/dotRing';
 
 function Layout({ children }) {
   return (
     <div>
+      <DotRing />
       <Header />
       <div className="spacer"></div>
       {children}
