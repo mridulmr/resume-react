9f42d2ea5bdaae87931a239ee295ccce875f2e39
akkuu <mridulmr@outlook.com>
2023-11-08 13:34:16 +0530

    updated the flow

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 import ScrollTop from '../elements/scrollTop';
-import 'swiper/css';
 import DotRing from '../elements/dotRing';
 
 function Layout({ children }) {
