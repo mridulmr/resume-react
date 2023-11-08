77264fc84f9edb06c293d23695120e823c14a454
akkuu <mridulmr@outlook.com>
2023-11-08 13:33:21 +0530

    Update scrollTop.js

--- src/elements/scrollTop.js
+++ src/elements/scrollTop.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function ScrollTop() {
   return (
-    <button className='scroll-container'>
+    <button
+      className="scroll-container"
+      onClick={() =>
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+      }
+    >
       <img src="https://moonex.ibthemespro.com/assets/img/chevron-w.png" />
     </button>
   );
