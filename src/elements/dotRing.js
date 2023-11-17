500c1d903683c3c5e0103a0f1f864cb039ae4ef5
Mridul Rathore <mridulmr@outlook.com>
2023-11-17 14:34:06 +0530

    trailing mouse animation

--- src/elements/dotRing.js
+++ src/elements/dotRing.js
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react';
 import useMousePosition from '../hooks/useMousePosition';
 
 const DotRing = () => {
+  const [trailX, setTrailX] = useState(0);
+  const [trailY, setTrailY] = useState(0);
   const { x, y } = useMousePosition();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTrailX(x);
+      setTrailY(y);
+    }, 200);
+  }, [x, y]);
+
   return (
     <>
       <div
-        style={{ left: `${x}px`, top: `${y}px` }}
+        style={{ left: `${trailX}px`, top: `${trailY}px` }}
         className="ring"
       ></div>
       <div
